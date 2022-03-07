0e7fec8efe0349519a264f5b4d88c652626d7bfc
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-03-07 18:52:02 +0100

    Style mobile popup

--- modals.js
+++ modals.js
@@ -1,101 +1,101 @@
-//create array of my all objects
+// create array of my all objects
 
 const data = [{
   project1: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-one.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-one.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   },
   project2: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-two.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-two.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   },
   project3: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-three.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-three.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   },
   project4: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-four.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-four.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   },
   project5: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-five.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-five.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   },
   project6: {
-    name:'Tonic',
-    tags:['html', 'css', 'javaScript'],
-    img:'./images/project-six.svg',
+    name: 'Tonic',
+    tags: ['html', 'css', 'javaScript'],
+    img: './images/project-six.svg',
     decription:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live:'https://tahenybelguith.github.io/my-identity/',
-    source:'https://github.com/TahenyBELGUITH/my-identity'
+    live: 'https://tahenybelguith.github.io/my-identity/',
+    source: 'https://github.com/TahenyBELGUITH/my-identity',
   }
-}]
+}];
 
-const container=document.querySelector('.popup-container');
-const seeProjectBtns = document.querySelectorAll('#see-btn') //output:array
-//create dynamic fonction : createModel()
-function createModel({name , tags , img ,description , live , source},index)
-{
- const div=document.createElement('div')
- div.id='popup';
- div.innerHTML=`
-            <h1>${name}</h1>
+const containerPopUp = document.querySelector('.popup-container');
+const seeProjectBtns = document.querySelectorAll('#see-btn'); // output:array
+// create create  fonction
+function createModel({
+  name, tags, img, description, live, source,
+}, index) {
+  const div = document.createElement('div');
+  div.id = 'popup';
+  div.innerHTML = `
+            <h1>${name}</h1> 
               <ul>
                 <li>HTML/CSS</li>
                 <li>Ruby on Rails</li>
                 <li>javaScript</li>
               </ul>
-            <img src="${img}" alt="project three">
+            <img src="./images/${img}" alt="project three">
             <p>${description}</p>
                 <button type="button"><a href="${live}">see Live </a><i class="fa-solid fa-arrow-trend-up"></i></button>
-                <button type="button"><a href="${source}">see source </a><i class="fa-brands fa-github"></i></button>`
-         container.appendChild(div);     
+                <button type="button"><a href="${source}">see source </a><i class="fa-brands fa-github"></i></button>`;
+  containerPopUp.appendChild(div);
 }
 
-function openPopUp()
-{
- container.classList.remove('display-none');
+function openPopUp() {
+  containerPopUp.classList.remove('display-none');
 }
 
-function closePopUp()
-{
- container.classList.add('display-none');
+function closePopUp() {
+  containerPopUp.classList.add('display-none');
 }
-//tag all btns
-for(let i=0 ; i<=seeProjectBtns.length;i++){
- seeProjectBtns[i].addEventListener('click', ()=>{
-   data.forEach((element) =>{
-     openPopUp();
-     createModel(element,i);
-   });
- })
-}
+// tag all btns
 
-//tag close btn
- closeBtn.addEventListener('click',closePopUp);
+data.forEach((element) => {
+  for (let i = 1; i <= seeProjectBtns.length; i++) {
+    seeProjectBtns[i].addEventListener('click', () => {
+      openPopUp();
+      createModel(element, i);
+    });
+  }
+});
+
+// tag close btn
+closeBtn.addEventListener('click', closePopUp);
