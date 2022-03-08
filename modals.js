f1135980fc77ca405fecea33c01a74a776ec3d7c
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-03-08 18:06:56 +0100

    Style mobile popup

--- modals.js
+++ modals.js
@@ -1,101 +1,112 @@
 // create array of my all objects
 
 const data = [{
-  project1: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-one.svg',
-    decription:
+  id: 1,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-one.svg',
+  description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  },
-  project2: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-two.svg',
-    decription:
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+{
+  id: 2,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-two.svg',
+  description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  },
-  project3: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-three.svg',
-    decription:
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+{ 
+  id: 3,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-three.svg',
+  description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  },
-  project4: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-four.svg',
-    decription:
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+{
+  id: 4,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-four.svg',
+  description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  },
-  project5: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-five.svg',
-    decription:
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+{ id: 5,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-five.svg',
+  description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  },
-  project6: {
-    name: 'Tonic',
-    tags: ['html', 'css', 'javaScript'],
-    img: './images/project-six.svg',
-    decription:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
-    live: 'https://tahenybelguith.github.io/my-identity/',
-    source: 'https://github.com/TahenyBELGUITH/my-identity',
-  }
-}];
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+{
+  id: 6,
+  name: 'project name goes here',
+  tags: ['html/css', 'Ruby on Rails', 'javaScript'],
+  img: './images/project-six.svg',
+  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled is simply ',
+  live: 'https://tahenybelguith.github.io/my-identity/',
+  source: 'https://github.com/TahenyBELGUITH/my-identity',
+},
+];
 
 const containerPopUp = document.querySelector('.popup-container');
 const seeProjectBtns = document.querySelectorAll('#see-btn'); // output:array
-// create create  fonction
+// create createModal  fonction
+
+const index = Object.keys(data);
+
 function createModel({
-  name, tags, img, description, live, source,
+name, tags, img, description, live, source,
 }, index) {
   const div = document.createElement('div');
   div.id = 'popup';
   div.innerHTML = `
             <h1>${name}</h1> 
               <ul>
-                <li>HTML/CSS</li>
-                <li>Ruby on Rails</li>
-                <li>javaScript</li>
+                <li>${tags[0]}</li>
+                <li>${tags[1]}</li>
+                <li>${tags[2]}</li>
               </ul>
-            <img src="./images/${img}" alt="project three">
-            <p>${description}</p>
+            <img src="${img}" alt="project image">
+             <p>${description}</p>
                 <button type="button"><a href="${live}">see Live </a><i class="fa-solid fa-arrow-trend-up"></i></button>
-                <button type="button"><a href="${source}">see source </a><i class="fa-brands fa-github"></i></button>`;
+                <button type="button"><a href="${source}">see source</a><i class="fa-brands fa-github"></i></button>`;
   containerPopUp.appendChild(div);
-}
-
-function openPopUp() {
-  containerPopUp.classList.remove('display-none');
 }
 
 function closePopUp() {
   containerPopUp.classList.add('display-none');
 }
-// tag all btns
+function openPopUp() {
+  containerPopUp.classList.remove('display-none');
+}
+
 
 data.forEach((element) => {
-  for (let i = 1; i <= seeProjectBtns.length; i++) {
+  for (let i = 0; i <= seeProjectBtns.length; i += 1) {
     seeProjectBtns[i].addEventListener('click', () => {
       openPopUp();
-      createModel(element, i);
+      createModel(element,i);
     });
   }
 });
 
 // tag close btn
+const closeBtn = document.querySelector('.close-popup');
 closeBtn.addEventListener('click', closePopUp);
+
+
+const projectsBtns =[];
+seeProjectBtns.forEach((element) => const {
+  id,} = element )
